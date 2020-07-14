6199d4089336a6f035fd863d2c85357c2a5a04ea
Flavio fernando da Silva <tchfernando@gmail.com>
2020-07-14 03:46:57 -0300

    Define snake moves

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -19,5 +20,26 @@ function createSnake(){
     }
 }
 
-createBG();
-createSnake();
+function initiateGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(initiateGame, 100);
