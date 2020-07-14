0c0be5dfdb5b28edf8ad304be04d175560308690
Flavio fernando da Silva <tchfernando@gmail.com>
2020-07-14 04:18:25 -0300

    Define directions and impede snake from vanishing

--- script.js
+++ script.js
@@ -20,11 +20,25 @@ function createSnake(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function initiateGame(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     createBG();
     createSnake();
 
-    let snakeX = snake[0].x; 
+    let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
@@ -40,6 +54,7 @@ function initiateGame(){
     }
 
     snake.unshift(newHead);
+
 }
 
 let game = setInterval(initiateGame, 100);
